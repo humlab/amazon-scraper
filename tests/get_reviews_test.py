2205124bd02fce47b3779e863c60189f2749a3ed
aibakeneko <aibakeneko@gmail.com>
2024-10-02 17:17:28 +0200

    Refactor test_get_reviews_test.py to use a fixture for the webdriver and add a test case for when there are no reviews

--- tests/get_reviews_test.py
+++ tests/get_reviews_test.py
@@ -1,24 +1,20 @@
-# import pytest
-# from selenium import webdriver
-# from selenium.webdriver.remote.webdriver import WebDriver
+import pytest
+from selenium import webdriver
 
-# from amazon_scraper.amazon_scraper import get_reviews, load_yaml
-
-# CONFIG = load_yaml("config/selectors.yaml")
+from amazon_scraper.amazon_scraper import get_reviews
 
 
-# @pytest.fixture
-# def driver():
-#     driver = webdriver.Firefox()
-#     yield driver
-#     driver.quit()
+@pytest.fixture
+def driver():
+    driver = webdriver.Firefox()
+    yield driver
+    driver.quit()
 
 
-# def test_get_reviews(driver):
+def test_get_reviews_when_no_reviews(driver):
+    asin = "B005MTXL46"
+    base_url = "https://www.amazon.com"
 
-#     asin = "B0CRHX3CL3"
-#     base_url = "https://www.amazon.com"
+    reviews = get_reviews(driver, base_url, asin, "positive")
 
-#     reviews = get_reviews(driver, base_url, asin, "positive")
-
-#     assert reviews
+    assert reviews
