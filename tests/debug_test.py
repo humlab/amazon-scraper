86609dd775cc4b06f9f1ebcf6ea3efd3e5f6a7d2
roger-mahler <roger.mahler@hotmail.com>
2024-10-14 09:24:01 +0200

    test: added debug test

---
+++ tests/debug_test.py
@@ -0,0 +1,16 @@
+from typing import Any
+
+import pytest
+
+from amazon_scraper.configuration.inject import ConfigStore, ConfigValue
+from amazon_scraper.scripts.main import scrape_workflow
+
+
+@pytest.mark.skip(reason="This is a debug test")
+def test_scrape():
+    ConfigStore.configure_context(source='config/config.yml')
+    options: dict[str, Any] = ConfigValue("options").resolve()
+    domain = "de"
+    keyword = "tefillin"
+    scrape_workflow(options, keyword, domain, force=True)
+    assert True
