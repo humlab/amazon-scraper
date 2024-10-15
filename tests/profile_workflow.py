177979ee4e9d0df798823c9c23ad3cc4e4768bb8
aibakeneko <aibakeneko@gmail.com>
2024-10-15 16:38:32 +0200

    Changed keyword

--- tests/profile_workflow.py
+++ tests/profile_workflow.py
@@ -6,5 +6,5 @@ from amazon_scraper.scripts.main import scrape_workflow
 ConfigStore.configure_context(source='tests/profile_config.yml')
 options: dict[str, Any] = ConfigValue("options").resolve()
 domain = "de"
-keyword = "prayer shawl"
+keyword = "laptop"
 scrape_workflow(options, keyword, domain, force=True)
