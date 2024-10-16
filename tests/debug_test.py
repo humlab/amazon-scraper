2c49c68794f436e1dc1dfec57a57be9937dce87a
aibakeneko <aibakeneko@gmail.com>
2024-10-16 16:49:52 +0200

    Skip debug test in test_scrape function

--- tests/debug_test.py
+++ tests/debug_test.py
@@ -6,7 +6,7 @@ from amazon_scraper.configuration.inject import ConfigStore, ConfigValue
 from amazon_scraper.scripts.main import scrape_workflow
 
 
-# @pytest.mark.skip(reason="This is a debug test")
+@pytest.mark.skip(reason="This is a debug test")
 def test_scrape():
     ConfigStore.configure_context(source='config/config.yml')
     options: dict[str, Any] = ConfigValue("options").resolve()
